5e01bd558d9b6a56d8dd10ec91e821d5c8fdfe46
Simon Duquette <simon.duquette@hotmail.com>
2021-05-25 15:38:18 -0400

    alterante spam using requests. Currently not working

--- ups_scam.py
+++ ups_scam.py
@@ -3,80 +3,36 @@ from selenium import webdriver
 import time
 import json
 import random
+import requests
+
+tracking_url = 'https://52-55-65-224.cprapid.com/tracking/en/gb/'
+address_url = 'https://18-190-156-86.cprapid.com/tracking/en/gb/address.php'
+payment_url = 'https://18-190-156-86.cprapid.com/tracking/en/gb/payment.php'
+confirmation_url = 'https://18-190-156-86.cprapid.com/tracking/en/gb/confirmation.php'
+data = {
+    'first_name': 'aaaaaaaaaaa',
+    'last_name': 'aaaaaaaaa',
+    'phone_number': '2222222222',
+    'dob': 'aaaaaaa',
+    'address': 'aaaaaaa',
+    'countryCode': 'DZ',
+    'city': 'aaaa',
+    'postal_code': 'aaaaa',
+    'recoverMyID': ''
+}
 
 
-while True:
-    rand = random.randint(0, 49)
-    with open('last_names.txt') as f:
-        last_name = f.readlines()[rand].strip('\n')
+response = requests.post(address_url, data=data).text
+print(response)
+time.sleep(50)
+data = {
+    'card_number': '5398706302348124',
+    'exp_date': '33/33',
+    'cvv': '333',
+    'email': 'dog@yopmail.com',
+    'recoverMyID':' '
+}
+response = requests.post(payment_url, data=data).text
+print(response)
 
-    rand = random.randint(0, 99)
-    with open('first_names.txt') as f:
-        first_name = f.readlines()[rand].strip('\n')
-
-    with open('visa.json') as f:
-        data = json.load(f)
-        random_CC = (data[rand]["CreditCard"]["CardNumber"])
-
-    with open('addresses.json') as f:
-        data = json.load(f)
-        address = (data[rand]["address"])
-        city    = (data[rand]["city"])
-        state   = (data[rand]["state"])
-        zip     = (data[rand]["zip"])
-
-
-    url = 'http://newsvillafilmy.com/ed/qaxlrlfncnrgeex'
-    driver = webdriver.Chrome()
-    driver.get(url)
-
-    new_delivery_button = driver.find_element_by_xpath('//*[@id="submitRecoverMyIDBtn"]')
-    new_delivery_button.click()
-    time.sleep(1)
-
-
-    first_name_field = driver.find_element_by_xpath('//*[@id="first_name"]')
-    last_name_field = driver.find_element_by_xpath('//*[@id="last_name"]')
-    phone_field = driver.find_element_by_xpath('//*[@id="phone_number"]')
-    dob_field = driver.find_element_by_xpath('//*[@id="dob"]')
-    address_field = driver.find_element_by_xpath('//*[@id="address"]')
-    counrty_field = driver.find_element_by_xpath('//*[@id="countryCode"]')
-    city_field = driver.find_element_by_xpath('//*[@id="city"]')
-    zip_field = driver.find_element_by_xpath('//*[@id="postal_code"]')
-
-    first_name_field.send_keys(first_name)
-    last_name_field.send_keys(last_name)
-
-    phone_field.send_keys(random.randint(1000000, 9999999))
-
-    rand_day = random.randint(1, 31)
-    rand_month = random.randint(1, 12)
-    rand_year = random.randint(1969,2000)
-
-    dob_field.send_keys(f"{rand_month}/{rand_day}/{rand_year}")
-    address_field.send_keys(address)
-    counrty_field.send_keys("United")
-    city_field.send_keys(city)
-    zip_field.send_keys(zip)
-    driver.find_element_by_xpath('//*[@id="submitRecoverMyIDBtn"]').click()
-
-    time.sleep(0.5)
-    cc_num = driver.find_element_by_xpath('//*[@id="card_number"]')
-    cc_exp = driver.find_element_by_xpath('//*[@id="exp_date"]')
-    cc_cvv = driver.find_element_by_xpath('//*[@id="cvv"]')
-    email  = driver.find_element_by_xpath('//*[@id="email"]')
-
-
-    rand_month = random.randint(1, 9)
-    rand_year  = random.randint(22, 36)
-    rand_email = random.randint(1969, 2000)
-    rand_cvv   = random.randint(100, 999)
-
-    cc_num.send_keys(random_CC)
-    cc_exp.send_keys(f"{rand_month:02d}{rand_year}")
-    cc_cvv.send_keys(rand_cvv)
-    email.send_keys(f"{first_name.lower()}.{last_name.lower()}{rand_email}@gmail.com")
-
-    time.sleep(0.5)
-    driver.find_element_by_xpath('//*[@id="submitRecoverMyIDBtn"]').click()
-    driver.close()
+#Does not send any information, is currently stuck at homepage. Cannot click button
